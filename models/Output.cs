7c8c30445ed224bceaf75bf0b8d9b9d8f12e81bd
Fatih ÖZDEMİR <fatihozdemir571@gmail.com>
2022-06-11 08:51:26 +0300

    ek

---
+++ models/Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP.models
+{
+    public class Output
+    {
+        public string NormalizdText { get; set; }
+    }
+}
---
+++ models/TextData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP.models
+{
+    public class TextData
+    {
+        private string text;
+
+        public string Text { get => text; set => text = value.ToLower(); }
+    }
+}
